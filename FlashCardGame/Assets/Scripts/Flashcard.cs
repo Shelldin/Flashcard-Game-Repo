be4aa738aae399afbe3d1d39c2b66f8dff6f9ba4
Shelldin <sheldon.bean@gmail.com>
2023-08-16 20:01:29 -0600

    created flashcard script

---
+++ FlashCardGame/Assets/Scripts/Flashcard.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Flashcard
+{
+    public string question;
+    public string answer;
+    
+    public bool openAnswer;
+    public bool caseSensitive;
+
+    public Flashcard(string newQuestion, string newAnswer, bool isOpenAnswer, bool isCaseSensitive)
+    {
+        question = newQuestion;
+        answer = newAnswer;
+
+        openAnswer = isOpenAnswer;
+        caseSensitive = isCaseSensitive;
+    }
+}
