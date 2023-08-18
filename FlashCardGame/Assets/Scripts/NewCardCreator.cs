c2da5f8bb33af95a9f3107796a67b96274d4407f
Shelldin <sheldon.bean@gmail.com>
2023-08-17 19:27:33 -0600

    Created NewCardCreator script

---
+++ FlashCardGame/Assets/Scripts/NewCardCreator.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewCardCreator : MonoBehaviour
+{
+    public FlashcardListManager flashcardListManager;
+
+    //empty game object that contains all the card creation UI elements
+    public GameObject creatorUIGameObject;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
