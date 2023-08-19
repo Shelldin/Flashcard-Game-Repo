a6ba976b6b9d8a06fd93dc9e88fdd0861f1a6efd
Shelldin <sheldon.bean@gmail.com>
2023-08-19 15:34:17 -0600

    Added methods to TogglesController to add activation exclusivity

--- FlashCardGame/Assets/Scripts/CreationTogglesController.cs
+++ FlashCardGame/Assets/Scripts/CreationTogglesController.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine;using UnityEngine.UI;
 
 public class CreationTogglesController : MonoBehaviour
 {
+    public Toggle isOpenAnswerToggle;
+    public Toggle isMultipleChoiceToggle;
+    public Toggle isCaseSensitiveToggle;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        isOpenAnswerToggle.isOn = true;
+        isMultipleChoiceToggle.isOn = false;
+        isCaseSensitiveToggle.isOn = false;
     }
 
     // Update is called once per frame
@@ -17,4 +23,30 @@ public class CreationTogglesController : MonoBehaviour
     }
     
     //makes it so open answer and multiple choice toggle cannot both be true at the same time
+    public void OpenAnswerToggleEvent()
+    {
+        if (isOpenAnswerToggle.isOn)
+        {
+            isMultipleChoiceToggle.isOn = false;
+            isCaseSensitiveToggle.interactable = true;
+        }
+        else if (!isOpenAnswerToggle.isOn)
+        {
+            isCaseSensitiveToggle.interactable = false;
+        }
+    }
+    
+    //makes it so open answer and multiple choice toggle cannot both be true at the same time
+    public void MultipleChoiceToggleEvent()
+    {
+        if (isMultipleChoiceToggle.isOn)
+        {
+            isOpenAnswerToggle.isOn = false;
+            isCaseSensitiveToggle.interactable = false;
+        }
+        else if (!isMultipleChoiceToggle.isOn)
+        {
+            isCaseSensitiveToggle.interactable = true;
+        }
+    }
 }
--- FlashCardGame/Assets/Scripts/NewCardCreator.cs
+++ FlashCardGame/Assets/Scripts/NewCardCreator.cs
@@ -7,21 +7,13 @@ using UnityEngine.UI;
 public class NewCardCreator : MonoBehaviour
 {
     public FlashcardListManager flashcardListManager;
+    public CreationTogglesController creationTogglesController;
     
     public TMP_InputField questionInput;
     public TMP_InputField answerInput;
-
-    public Toggle isOpenAnswerToggle;
-    public Toggle isMultipleChoiceToggle;
-    public Toggle isCaseSensitiveToggle;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        isOpenAnswerToggle.isOn = true;
-        isMultipleChoiceToggle.isOn = false;
-        isCaseSensitiveToggle.isOn = false;
-    }
+    
+  
 
     // Update is called once per frame
     void Update()
