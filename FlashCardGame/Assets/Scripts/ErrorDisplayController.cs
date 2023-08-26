5bcb597315d1e7ecf03e673cac289a7298d58b52
Shelldin <sheldon.bean@gmail.com>
2023-08-25 18:01:03 -0600

    Added to coroutine to errordisplaycontroller

--- FlashCardGame/Assets/Scripts/ErrorDisplayController.cs
+++ FlashCardGame/Assets/Scripts/ErrorDisplayController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -7,9 +8,27 @@ public class ErrorDisplayController : MonoBehaviour
 {
     public TextMeshPro errorText;
 
+    public float errorCountdownTime = 5f;
+
+    public Coroutine errorDisplayCountdownCo;
+    
+
+    private void Start()
+    {
+        //errorDisplayCountdownCo = StartCoroutine()
+    }
+
     //update errorText with errorMessage and display the text for the user 
     public void DisplayErrorText(string errorMessage)
     {
         errorText.text = errorMessage;
+    }
+
+    //NOTE TO SELF: FINISH THE COROUTINE
+    
+    private IEnumerator ErrorDisplayCoroutine()
+    {
+        errorText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(errorCountdownTime);
     }
 }
