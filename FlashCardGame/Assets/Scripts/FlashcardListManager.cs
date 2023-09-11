5ba7f6a862ce6fdb5365d9f212b99911080a3c63
Shelldin <sheldon.bean@gmail.com>
2023-09-11 16:43:33 -0600

    randomly select index for flashcard list

--- FlashCardGame/Assets/Scripts/FlashcardListManager.cs
+++ FlashCardGame/Assets/Scripts/FlashcardListManager.cs
@@ -16,7 +16,6 @@ public class FlashcardListManager : MonoBehaviour
 
     public bool isOpenAnswer;
     public bool isCaseSensitive;
-    
 
     //add new flash card to list based on parameters set by user
     public void AddNewFlashcard()
@@ -24,9 +23,17 @@ public class FlashcardListManager : MonoBehaviour
         flashcards.Add(new Flashcard(newQuestion, newAnswer, isOpenAnswer, isCaseSensitive));
     }
 
-    public int SelectRandomFlashcard()
+    //set a random int
+    private int RandomFlashcardInt()
     {
         int randomFlashcardsIndex = Random.Range(0, flashcards.Count);
         return randomFlashcardsIndex;
+    }
+
+    // select a random flashcard from the list based on the random index selected
+    public void SelectRandomFlashcard()
+    {
+        int randomIndex = RandomFlashcardInt();
+        currentFlashcard = flashcards[randomIndex];
     }
 }
